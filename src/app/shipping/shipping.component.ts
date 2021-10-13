98d627ed7d81a04006543e1e863f51a452b14ced
Josh Davis <jldavis33@yahoo.com>
2021-10-13 12:33:47 -0500

    Building the `shipping` view template - injects CartService into ShippingComponent to get price data

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CartService} from "../cart.service";
 
 @Component({
   selector: 'app-shipping',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ShippingComponent {
 
-  constructor() { }
+  constructor(
+      private cartService: CartService
+  ) { }
+
+  shippingCosts = this.cartService.getShippingPrices();
 
 }
